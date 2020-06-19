0c57457e26b93655a4ff7cb816a5a94a629fec49
LIBERT4D <67157623+LIBERT4D@users.noreply.github.com>
2020-06-19 11:29:54 -0400

    Update config.js

--- static/config.js
+++ static/config.js
@@ -1,22 +1,22 @@
-// ***********************************************************************************
+﻿// ***********************************************************************************
 // ID of the Google spreadsheet
 // ***********************************************************************************
 // Make sure your spreadsheet is published (File > Publish to the web…)
-var spreadsheetId = "1P9wMb9e0YbhcOua9RQeRCJjllf_L77uV-7i4Q0Yor0o";
+var spreadsheetId = "15f6OddSOhgkzZ9kUKVEtdO_-G4Of3V26uctf5R7uE5I";
 
 // ***********************************************************************************
 // Your FC and IGN
 // ***********************************************************************************
-var friendCode = "4597-0550-9417";
-var inGameName = "Richie";
+var friendCode = "7463-4859-4002";
+var inGameName = "Rudo";
 
 // ***********************************************************************************
 // URL of your Reddit/Twitter account
 // ***********************************************************************************
-var contactUrl = "http://reddit.com/u/richi3f";
+var contactUrl = "http://reddit.com/u/LIBERT4D";
 
 // ***********************************************************************************
 // URL of your PGL trainer icon (optional)
 // ***********************************************************************************
 // Download it from the Pokémon Global Link and re-upload to an image-sharing website
-var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png";
+var trainerIconUrl = "";
